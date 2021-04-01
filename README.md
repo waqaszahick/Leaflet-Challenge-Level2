c26728633ed4c92abdaf6364761f75517539ac8b
Waqas Zahick <waqaszahick@hotmail.com>
2021-04-01 18:36:38 +1100

    Fixing the browser view...

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-# Leaflet Challenge
-Level 1: Basic Visualization
+# Leaflet-Challenge
+Level 2: More Data
 
 This is my overall 16th, and first assignment on visualization data with "leaflet". Regarding the background of this assignment, it is about the United States Geological Survey (USGS). The USGS is responsible for providing scientific data about natural hazards, the health of our ecosystems and environment; and the impacts of climate and land-use change. Their scientists develop new methods and tools to supply timely, relevant, and useful information about the Earth and its processes.
 I was required to build a new set of tools that will allow user(s) to visualize earthquake data. The USGS collect a massive amount of data from all over the world each day. My assignment was to enable that data to be visualized in order to allow them (The USGS) to better educate the public and other government organizations, and hopefully secure more funding on issues facing our planet.
-This is a two-level or two-part assignment. In level one i.e. basic visualization, I performed the following tasks:
--	Visualized an earthquake data set.
--	Fetched the data-set from the USGS GeoJSON Feed page for visualization.
--	Imported and visualized the data.
--	Created a map using “Leaflet” that plots all of the earthquakes from the data-set based on their longitude and latitude.
--	Made my data markers reflect the magnitude of the earthquake in their size and colour.
--	Made earthquakes with higher magnitudes appear larger and darker in colour.
--	Included pop-ups that provide additional information about the earthquake when a marker is clicked.
--	Created a legend that provides context for the map data.
+This is a two-level or two-part assignment. As a part of level two of this assignment, I was required to plot a second data-set on the map to illustrate the relationship between tectonic plates and seismic activity. I had to pull-in a second data set from https://github.com/fraxen/tectonicplates, and visualize it alongside my original set of data.
+In this step (level two) of my assignment, I performed the following tasks:
+-	Plotted a second data-set on the map.
+-	Added a number of base maps to choose from, as well as separate-out two different data-sets into overlays that could be turned on and off independently.
+-	Added layer controls to the map.
--- js/logic.js
+++ js/logic.js
@@ -1,74 +1,114 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-// Define streetmap and darkmap layers
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
+// Selectable backgrounds of our map - tile layers:
+// grayscale background.
+var graymap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+
+// satellite background.
+var satellitemap_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+
+// outdoors background.
+var outdoors_background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1IjoibWFudWVsYW1hY2hhZG8iLCJhIjoiY2ppczQ0NzBtMWNydTNrdDl6Z2JhdzZidSJ9.BFD3qzgAC2kMoEZirGaDjA");
+
+// map object to an array of layers we created.
+var map = L.map("mapid", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [graymap_background, satellitemap_background, outdoors_background]
 });
 
-var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "dark-v10",
-  accessToken: API_KEY
-});
+// adding one 'graymap' tile layer to the map.
+graymap_background.addTo(map);
 
-// Define a baseMaps object to hold our base layers
+// layers for two different sets of data, earthquakes and faultlines.
+var faultlines = new L.LayerGroup();
+var earthquakes = new L.LayerGroup();
+earthquakes = L.markerClusterGroup();
+
+// base layers
 var baseMaps = {
-  "Street Map": streetmap,
-  "Dark Map": darkmap
+  Grayscale: graymap_background,
+  Outdoors: outdoors_background,
+  Satellite: satellitemap_background,
 };
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  earthquakeData = data.features;
-  
-  var earthquakes = L.markerClusterGroup();
-  L.geoJSON(earthquakeData, {
-    pointToLayer: function(earthquakeData, latlng) {
-      return new L.CircleMarker(latlng, {
-        radius: getRadius(earthquakeData.properties.mag),
-        color: getColor(earthquakeData.properties.mag),
-        stroke: true,
-        weight: 0.5,
-        opacity: 1,
-        fillOpacity: 1,
-        fillcolor: getColor(earthquakeData.properties.mag)
-      }).bindPopup("Magnitude: " + earthquakeData.properties.mag + "<br>Location: " + earthquakeData.properties.place);
-    }}).addTo(earthquakes);
-  
-    
-  // Sending our earthquakes layer to the createMap function
-  
-  // Create overlay object to hold our overlay layer
-  
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-  
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [streetmap, earthquakes]
-  });
-  
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+// overlays 
+var overlayMaps = {
+  "Fault Lines": faultlines,
+  "Earthquakes": earthquakes
+};
 
-  // Set up the d
-  var legend = L.control({ position: "bottomright" });
+// control which layers are visible.
+L
+  .control
+  .layers(baseMaps, overlayMaps)
+  .addTo(map);
+
+// retrieve earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+
+
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // Define the color of the marker based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    switch (true) {
+      case magnitude > 5:
+        return "#ea2c2c";
+      case magnitude > 4:
+        return "#ea822c";
+      case magnitude > 3:
+        return "#ee9c00";
+      case magnitude > 2:
+        return "#eecc00";
+      case magnitude > 1:
+        return "#d4ee00";
+      default:
+        return "#98ee00";
+    }
+  }
+
+  // define the radius of the earthquake marker based on its magnitude.
+
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 3;
+  }
+
+  // add GeoJSON layer to the map
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(map);
+
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+
   legend.onAdd = function() {
     var div = L
       .DomUtil
@@ -91,29 +131,21 @@ d3.json(queryUrl, function(data) {
     }
     return div;
   };
-  legend.addTo(myMap);
-// Define the color of the marker based on the magnitude of the earthquake.
-function getColor(magnitude) {
-  switch (true) {
-    case magnitude > 5:
-      return "#ea2c2c";
-    case magnitude > 4:
-      return "#ea822c";
-    case magnitude > 3:
-      return "#ee9c00";
-    case magnitude > 2:
-      return "#eecc00";
-    case magnitude > 1:
-      return "#d4ee00";
-    default:
-      return "#98ee00";
-  }
-}
-function getRadius(magnitude) {
-  if (magnitude === 0) {
-    return 1;
-  }
 
-  return magnitude * 3;
-}
+
+  legend.addTo(map);
+
+  // retrive Tectonic Plate geoJSON data.
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
+    function(platedata) {
+ 
+      L.geoJson(platedata, {
+        color: "orange",
+        weight: 3
+      })
+      .addTo(faultlines);
+
+      // add the faultlines layer to the map.
+      faultlines.addTo(map);
+    });
 });
